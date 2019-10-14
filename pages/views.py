739769d275b4b718ab3ff957d32ab63d9507877f
Rubel Ahmed <rubelc914@gmail.com>
2019-10-15 03:59:31 +0600

    first commit

---
+++ pages/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from listings.models import Listing
+from realtors.models import Realtor
+
+def index(request):
+    listings=Listing.objects.order_by('list_date').filter(is_published=True)[:3]
+    context = {
+        'listings':listings
+    }
+    return render(request,'pages/index.html', context)
+
+
+def about(request):
+
+    realtors=Realtor.objects.order_by('-hire_date')
+    mpv_realtor=Realtor.objects.all().filter(is_mvp=True)
+    context={
+        'realtors':realtors,
+        'mpv_realtor': mpv_realtor
+    }
+    
+    return render(request,'pages/about.html', context)
+
+
